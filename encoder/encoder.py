25531997e9f3d0fd2534ba7610396e7739e626df
Oscar Candido <oscarcandido@me.com>
2018-05-22 08:21:20 -0300

    Cálculos básicos das velocidades lineares e angulares e deslocamento para o encoder

---
+++ encoder/encoder.py
@@ -0,0 +1,33 @@
+# pylint: disable=C0321, C0103, C0111, E1101, W0621, I0011
+# encoding: UTF-8
+import math
+import numpy as np
+
+#calcula a velocidade angular
+#Parâmetros
+#   Wr = velocidade angular da roda direita
+#   Wl = velocidade angular da roda esquerda
+#   r  = Raio das rodas
+#   L  = Distância entre as rodas
+def AngSpeed(Wr,Wl,r,L)
+    W = (Wr+Wl)*r/L
+    return W
+
+
+#calcula a velocidade linear
+#Parâmetros
+#   Wr = velocidade angular da roda direita
+#   Wl = velocidade angular da roda esquerda
+#   r  = Raio das rodas
+def LinSpeed(Wr,Wl,r)
+    S = (Wr+Wl)*r/2
+    return S
+
+
+#Calcula o linear
+def LinearDesloc(old_V,V,Ts)
+    LinDes = Ts* (PrW + W) /2
+    return LinDes
+
+
+
